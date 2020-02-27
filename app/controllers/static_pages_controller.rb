a992577ee0a9c03347d30efb86beb68c8a07f339
sushengy <sushengy@grinnell.edu>
2020-02-26 22:03:51 -0600

    hw6: autotest

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    @num_pic = 5
+    @index_min = 0
+    @index_max = @num_pic - 1
+    @index = rand(@num_pic)
   end
 
   def help
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,27 +2,49 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @base_title = "Memepedia - For Meme Lovers"
+  end
+
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Memepedia - For Meme Lovers"
+    assert_select "title", "#{@base_title}"
+  end
+
+  test "picture index should not be nil" do
+    get root_path
+    assert_response :success
+    assert_not_nil assigns (:index)
+  end
+
+  test "picture index should not be empty" do
+    get root_path
+    assert_response :success
+    assert_not_empty (:index)
+  end
+
+  test "picture index should be in range" do
+    get root_path
+    assert_response :success
+    assert_includes (assigns :index_min)..(assigns :index_max), (assigns :index)
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Memepedia - For Meme Lovers | Help"
+    assert_select "title", "#{@base_title} | Help"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "Memepedia - For Meme Lovers | About"
+    assert_select "title", "#{@base_title} | About"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Memepedia - For Meme Lovers | Contact"
+    assert_select "title", "#{@base_title} | Contact"
   end
 end
