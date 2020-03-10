2d5bb39232d782500c80a917e3660fa158a96a53
sushengy <sushengy@grinnell.edu>
2020-03-10 00:05:10 -0500

    hw7 update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# exmaple users
 for i in 1..10 do
   User.create(name: "User ##{i}", email: "user#{i}@example.com")
 end
